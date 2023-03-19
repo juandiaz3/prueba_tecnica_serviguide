7cb6b8c34961b117c59e017bdbefe173802956d8
juandiaz3 <juandisan80@yahoo.es>
2023-03-20 00:18:48 +0100

    Create repository. Add dependencies

---
+++ src/test/java/com/serviguide/prueba/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviguide.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
